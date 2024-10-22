9722e4033de8beee440a9697398d1af248171cc8
Ezequiel Miranda <mirandaezequiel522@gmail.com>
2024-10-21 22:53:51 -0300

    update

---
+++ Tasky.AIModel/InputData.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ML.Data;
+
+namespace Tasky.AIModel
+{
+    public class InputData
+    {
+        [LoadColumn(0)]
+        public string Asunto { get; set; }
+        [LoadColumn(1)]
+        public string Mensaje { get; set; }
+        [LoadColumn(2)]
+        public string Urgencia { get; set; }
+
+    }
+}
