f12e6b500ce04b77c8aa6a39d25ff9635f47c40d
PEDRO RAMON OSNAGHI <posnaghi@alumno.unlam.edu.ar>
2024-10-25 17:13:34 -0300

    las commit

--- Tasky.Datos/obj/Debug/net6.0/Tasky.Datos.AssemblyInfo.cs
+++ Tasky.Datos/obj/Debug/net6.0/Tasky.Datos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tasky.Datos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f48f021322568a74c8200488369252b105679b4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4bac91184f8e3e5fcf9b0fdbccffc7d86562632")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tasky.Datos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tasky.Datos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
