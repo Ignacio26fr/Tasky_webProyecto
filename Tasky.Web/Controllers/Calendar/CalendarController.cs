8ff9260d1fc77aaf7afc765971f1850c27d51b87
Ignacio26fr <igfranco@alumno.unlam.edu.ar>
2024-11-04 21:49:39 -0300

    add calendar

---
+++ Tasky.Web/Controllers/Calendar/CalendarController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tasky.Logica.Calendar;
+using Tasky.Logica.Session;
+
+namespace Tasky.Web.Controllers.Calendar
+{
+    public class CalendarController : Controller
+    {
+
+
+        private readonly IGoogleCalendarService _googleCalendarService;
+
+
+
+        public CalendarController(IGoogleCalendarService googleCalendarService)
+        {
+            _googleCalendarService = googleCalendarService;
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var calendarUrl = await _googleCalendarService.RedirectToGoogleCalendar();
+                return Redirect(calendarUrl);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                return View("Error", new { message = "No se pudo redirigir a Google Calendar." });
+            }
+        }
+    }
+}
+    
+
